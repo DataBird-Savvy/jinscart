7ad56c2c9519bb4bc2d2ec59bc74a3fe9e57beb9
jiyamary <mejiyamaryjoseph@gmail.com>
2026-04-22 13:35:18 +0530

    order layout fininshed

--- jinscart/customers/views.py
+++ jinscart/customers/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_account(request):
+    return render(request, "account.html")
+
+def detail_customer(request):
+    return render(request, "detail_customer.html")
+
