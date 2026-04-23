c2d259376e585db492159ce6e8487ebf48d546e6
jiyamary <mejiyamaryjoseph@gmail.com>
2026-04-23 17:19:55 +0530

    product module updated

--- jinscart/products/views.py
+++ jinscart/products/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from .models import Product
+from django.core.paginator import Paginator
 
 def index(request):
     return render(request, 'index.html')
 def list_products(request):
-    return render(request, 'products.html')
+    product_list=Product.objects.all()
+    paginator = Paginator(product_list, 1)
+    page_number=1
+    if request.GET.get('page'):
+        page_number=int(request.GET.get('page', 1))
+
+    
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'products.html',{'products':page_obj})
 def detail_product(request):
     return render(request, 'product_detail.html')
 
